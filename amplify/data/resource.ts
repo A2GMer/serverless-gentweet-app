73bc26413dd25bb4b5dbd41fcc153a1306e45f63
A2GMer <52153162+A2GMer@users.noreply.github.com>
2024-12-31 11:03:14 +0900

    fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { getTrendsFunction } from "../getTrends-function/resource"
+// import { getTrendsFunction } from "../getTrends-function/resource"
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
